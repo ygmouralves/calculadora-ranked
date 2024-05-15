9cf090b5a24e61e7533b4672754e94d645158efd
Ygor Moura <ssygor.mourass@gmail.com>
2024-05-15 09:08:06 -0300

    Adicionando arquivos da calculadora

---
+++ desafio2.js
@@ -0,0 +1,31 @@
+function calcularNivel(vitorias, derrotas) {
+  let saldoVitorias = vitorias - derrotas;
+  let nivel;
+
+  if (vitorias < 10) {
+    nivel = "Ferro";
+  } else if (vitorias >= 10 && vitorias <= 20) {
+    nivel = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+
+  return { saldoVitorias, nivel };
+}
+
+// Exemplo de uso:
+let vitorias = 75;
+let derrotas = 20;
+
+let resultado = calcularNivel(vitorias, derrotas);
+console.log(
+  `O Herói tem saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`
+);
